26284c607293a306d9124657e6a5ca41a2565274
dhruvpatel <pate5491@shernet.sheridancollege.ca>
2024-02-22 11:34:56 -0500

    added files

---
+++ ProjectMidterm/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectMidterm;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ ProjectMidterm/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectMidterm;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
